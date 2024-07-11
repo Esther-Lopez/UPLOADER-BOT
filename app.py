b2b7d25202f2adbd400f753fb881742ae50c6586
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2024-07-11 15:02:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    app.run()
     # Run the Flask app on port 80
-    app.run(host='0.0.0.0', port=80)
+    #app.run(host='0.0.0.0', port=80)
